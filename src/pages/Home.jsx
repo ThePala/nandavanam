86bf07a56a6b424a5421a9790eb116abd4f9cc8c
ThePala <palaadithyan@gmail.com>
2025-10-05 11:24:26 +0530

    added new section, and new images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,6 +39,19 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor i
             </p>
           </div>
         </section>
+
+        {/* Section 3 */}
+        <section
+          className="home-hero section-divider"
+          style={{ backgroundImage: "url('/images/poopalagai.JPG')" }}
+        >
+          <div className="home-hero-content">
+            <h2>Why are Nandavanams in decline?</h2>
+            <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </section>
       </main>
 
     </div>
